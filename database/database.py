19c1b0bd0ba29c94a28b18ebafae23a1a8ae1686
Bodigi.Tejeswara-rao <bodigiteja@gmail.com>
2026-07-08 12:59:58 +0530

    connected to super base

--- database/database.py
+++ database/database.py
@@ -1,9 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:2902@localhost:5432/scientific_collaboration"
-
-engine = create_engine(DATABASE_URL)
+# DATABASE_URL = "postgresql://postgres:Tejeswararao@db.ejjalcfggrwadhhzatem.supabase.co:5432/postgres"
+DATABASE_URL = (
+    "postgresql+psycopg2://"
+    "postgres.ejjalcfggrwadhhzatem:Tejeswararao@"
+    "aws-1-ap-northeast-2.pooler.supabase.com:5432/postgres"
+    "?sslmode=require"
+)
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"}
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
