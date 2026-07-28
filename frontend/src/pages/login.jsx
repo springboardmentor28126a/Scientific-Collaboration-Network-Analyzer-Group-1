01c95f471424e071b7102d977b175415814e6bde
pankajsharma223654 <psharma16996@gmail.com>
2026-07-28 16:45:58 +0530

    Fixed routing and UI improvements

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -14,7 +14,7 @@ function Login() {
         email,
         password,
       });
-
+console.log(response.data);
       localStorage.setItem("token", response.data.access_token);
 
       alert("Login Successful");
@@ -30,37 +30,111 @@ function Login() {
   };
 
   return (
-    <div style={{ textAlign: "center", marginTop: "100px" }}>
-      <h1>Scientific Collaboration Network Analyzer</h1>
+    <div
+      style={{
+        height: "100vh",
+        background: "linear-gradient(135deg,#2563eb,#1e3a8a)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <div
+        style={{
+          width: "420px",
+          background: "white",
+          padding: "40px",
+          borderRadius: "15px",
+          boxShadow: "0 10px 30px rgba(0,0,0,0.2)",
+          textAlign: "center",
+        }}
+      >
+        <h1
+  style={{
+    color: "#0931a7",
+    fontSize: "42px",
+    lineHeight: "1.2",
+    marginBottom: "10px",
+    fontWeight: "bold",
+  }}
+>
+  Scientific Collaboration
+</h1>
 
-      <h2>Login</h2>
+<p
+  style={{
+    color: "#000000",
+    marginBottom: "30px",
+    fontSize: "20px",
+  }}
+>
+  Network Analyzer
+</p>
 
-      <input
-        type="email"
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
+        <h2 style={{ marginBottom: "25px" }}>Login</h2>
 
-      <br /><br />
+        <input
+          type="Email"
+          placeholder="Enter Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          style={{
+            width: "100%",
+            padding: "14px",
+            marginBottom: "15px",
+            borderRadius: "8px",
+            border: "1px solid #f71414",
+            fontSize: "15px",
+            boxSizing: "border-box",
+          }}
+        />
 
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
+        <input
+          type="password"
+          placeholder="Enter Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          style={{
+            width: "100%",
+            padding: "14px",
+            marginBottom: "25px",
+            borderRadius: "8px",
+            border: "1px solid #f30505",
+            fontSize: "15px",
+            boxSizing: "border-box",
+          }}
+        />
 
-      <br /><br />
+        <button
+          onClick={handleLogin}
+          style={{
+            width: "100%",
+            padding: "14px",
+            background: "#010204",
+            color: "white",
+            border: "none",
+            borderRadius: "8px",
+            fontSize: "16px",
+            cursor: "pointer",
+          }}
+        >
+          Login
+        </button>
 
-      <button onClick={handleLogin}>
-        Login
-      </button>
-
-      <p>
-        Don't have an account?{" "}
-        <Link to="/signup">Signup</Link>
-      </p>
+        <p style={{ marginTop: "20px" }}>
+          Don't have an account?{" "}
+          <Link
+            to="/signup"
+            style={{
+              color: "#2556eb",
+              fontWeight: "bold",
+              textDecoration: "none",
+            }}
+          >
+            Signup
+          </Link>
+        </p>
+      </div>
     </div>
   );
 }
