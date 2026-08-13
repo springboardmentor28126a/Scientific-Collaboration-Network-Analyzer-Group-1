8def6793cc88549e2a3d4d674d68d0af79ad653e
Tarunika-Kumaravel <tarunika1411@gmail.com>
2026-08-13 15:51:08 +0530

    Merge Docker setup and citation enhancements

--- backend/main.py
+++ backend/main.py
@@ -152,6 +152,9 @@ app.add_middleware(
         "http://localhost:5177",
         "http://127.0.0.1:5177",
 
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
+
     ],
     allow_credentials=True,
     allow_methods=["*"],
